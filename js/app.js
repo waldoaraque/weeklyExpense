1b4b3a6cf62eb46479c5c10ab2440397574a6711
waldev <waldoaraque10@gmail.com>
2020-04-03 22:01:00 +0200

    Adding handlerMessage method in the interface class

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 //Vars
 const budgetUser = prompt('¿Cúal es tu presupuesto semanal?')
+const form = document.getElementById('agregar-gasto')
 let quantityBudget
 
 console.log(budgetUser)
@@ -16,6 +17,39 @@ class Budget {
     }
 }
 
+class Interface {
+    constructor() {}
+
+    setBudget(quantity) {
+        const budgetSpan = document.querySelector('span#total')
+        const leftoverSpan = document.querySelector('span#restante')
+
+        budgetSpan.innerHTML = `${quantity}`
+        leftoverSpan.innerHTML = `${}`
+    }
+
+    handlerMessage(m, t) {
+        const divMessage = document.createElement('div')
+        divMessage.classList.add('text-center', 'alert')
+
+        if (t === 'error') {
+            divMessage.classList.add('alert-danger')
+        } else {
+            divMessage.classList.add('alert-success')
+        }
+
+        divMessage.appendChild(document.createTextNode(m))
+
+        document.querySelector('.primario').insertBefore(divMessage, form)
+
+        setTimeout(() => {
+            document.querySelector('.primario .alert').remove()
+        }, 3000)
+
+        
+    }
+}
+
 
 //Event Listeners
 document.addEventListener('DOMContentLoaded', () => {
@@ -23,5 +57,21 @@ document.addEventListener('DOMContentLoaded', () => {
         window.location.reload()
     } else {
         quantityBudget = new Budget(budgetUser)
+        const ui = new Interface()
+        ui.setBudget(quantityBudget.budget)
+    }
+})
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const expenseName = document.querySelector('#gasto').value 
+    const expreseQuantity = document.querySelector('#cantidad').value 
+
+    const ui = new Interface()
+
+    if (expenseName === '' || expenseQuantity === '') {
+        ui.handlerMessage('Hubo un error', 'error')
+    } else {
+        
     }
 })
